f7977b7cbcd927a251221fb7b06d6758f9bd3397
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-10 14:14:38 -0300

    Fazendos os primeiros exercicios do dia 1

---
+++ day1Ex1.js
@@ -0,0 +1,25 @@
+let challenge ='30 Days Of JavaScript';
+console.log(challenge);
+console.log(challenge.length);
+
+
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+
+console.log(challenge.substring(0,2));
+console.log(challenge.substring(3,21));
+
+
+console.log(challenge.includes('Script'));
+
+console.log(challenge.split(' '));
+
+console.log(challenge.replace("JavaScript", "Python"));
+
+console.log(challenge.charAt(15));
+
+console.log(challenge.charCodeAt("j"));
+
+console.log(challenge.indexOf("a"));
+
+console.log(challenge.lastIndexOf("a"));
