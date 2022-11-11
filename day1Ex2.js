a503a9f7f27f09b666c85093c3272de91ec929a6
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-11 14:19:49 -0300

    trasnformando frase em array

--- day1Ex2.js
+++ day1Ex2.js
@@ -16,12 +16,13 @@ console.log(palavraAmor.length); //3
 
 console.log(palavraPorque.length); //3
 
-//Limpe o texto e encontre a palavra mais frequente (dica, use substituir e expressões regulares).
+//3. Limpe o texto e encontre a palavra mais frequente (dica, use substituir e expressões regulares).
 
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
 sentenceClear = sentence.replace(/[%-&-#-@]/g, '');
 
+sentenceClear = sentenceClear.split(' ');
 
 console.log(sentenceClear)
 
