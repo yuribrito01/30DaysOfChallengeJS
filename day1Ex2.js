982be85b175f638d94035c467f1ebb82ba9453e8
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-11 13:33:04 -0300

    Limpando a frase 2.3

--- day1Ex2.js
+++ day1Ex2.js
@@ -12,7 +12,17 @@ console.log(palavraAmor.length); //3
 //'Você não pode terminar uma frase com porque porque porque é uma conjunção'
  let frase2 = 'Você não pode terminar uma frase com porque porque porque é uma conjunção';
 
- palavraPorque = frase2.match(/porque/gi);  //[ 'amor', 'amor', 'amor' ]
+ palavraPorque = frase2.match(/porque/gi);  //[ 'porque', 'porque', 'porque' ]
 
 console.log(palavraPorque.length); //3
+
+//Limpe o texto e encontre a palavra mais frequente (dica, use substituir e expressões regulares).
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+
+sentenceClear = sentence.replace(/[%-&-#-@]/g, '');
+
+
+console.log(sentenceClear)
+
 
