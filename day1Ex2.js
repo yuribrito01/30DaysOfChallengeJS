45ff4a4234604bbaaa1a0fce8c8c11d2c5878084
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-11 12:51:38 -0300

    Adicionando exercicio 2.2

--- day1Ex2.js
+++ day1Ex2.js
@@ -1,4 +1,4 @@
-//'O amor é a melhor coisa deste mundo. Alguns encontraram seu amor e alguns ainda estão procurando por seu amor.'
+//1.'O amor é a melhor coisa deste mundo. Alguns encontraram seu amor e alguns ainda estão procurando por seu amor.'
 //Conte o número de palavras amor nesta frase.
 
 let frase = 'O amor é a melhor coisa deste mundo. Alguns encontraram seu amor e alguns ainda estão procurando por seu amor.';
@@ -6,3 +6,13 @@ let frase = 'O amor é a melhor coisa deste mundo. Alguns encontraram seu amor e
 palavraAmor = frase.match(/amor/gi);  //[ 'amor', 'amor', 'amor' ]
 
 console.log(palavraAmor.length); //3
+
+
+//2. Use match() para contar o número de todos porque na seguinte frase: 
+//'Você não pode terminar uma frase com porque porque porque é uma conjunção'
+ let frase2 = 'Você não pode terminar uma frase com porque porque porque é uma conjunção';
+
+ palavraPorque = frase2.match(/porque/gi);  //[ 'amor', 'amor', 'amor' ]
+
+console.log(palavraPorque.length); //3
+
