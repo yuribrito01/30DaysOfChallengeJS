32ab69f2debb0b5e5f2462d2de493eed2e109ddc
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-17 13:26:16 -0300

    Ajustes finais

--- day1Ex2.js
+++ day1Ex2.js
@@ -20,11 +20,10 @@ console.log(palavraPorque.length); //3
 
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
-let sentenceClear = sentence.replace(/[%-&-#-@]/g, ''); //
+let sentenceClear = sentence.replace(/[%-&-#-@]/g, ''); //I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting 
+                                                        //than any other jobs Does this motivate you to be a teacher! This DaysOfJavaScript is also the result of love of teaching
 
-//sentenceClear = sentenceClear.split(' ');
-
-console.log(sentenceClear) 
+sentenceClear = sentenceClear.split(' ');
 
 // 4. Calcule a renda anual total da pessoa extraindo os números do texto a seguir.
 // 'Ele ganha 5.000 euros de salário por mês, 10.000 euros de bônus anual, 15.000 euros de cursos online por mês.'
