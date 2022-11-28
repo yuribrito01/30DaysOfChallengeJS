d2e9acaabce65947e71829d3bdb29fd1c4df0eb6
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-28 09:32:01 -0300

    Terminando exercicicios do dia 5 level 2

--- day5Lvl2.js
+++ day5Lvl2.js
@@ -15,4 +15,32 @@ shoppingCart.push('Sugar');// 'Meat', 'Milk', 'Coffee', 'Tea', 'Honey', 'Sugar'
 //Remova o mel
 const positionTea = shoppingCart.indexOf('Tea');
 shoppingCart.splice(positionTea,1);
-console.log(shoppingCart);
+console.log(shoppingCart); //'Meat', 'Milk', 'Coffee', 'Honey', 'Sugar' 
+//modificar honey para 'cheese'
+const positionHoney = shoppingCart.indexOf('Honey')
+shoppingCart[positionHoney] = 'Cheese';
+console.log(shoppingCart);//[ 'Meat', 'Milk', 'Coffee', 'Cheese', 'Sugar' ]
+
+//Na matriz de países, verifique se 'Ethiopia' existe na matriz, se existir, imprima 'ETHIOPIA'. Se não existir, adicione à lista de países.
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+  countries.includes('Ethiopia') ? console.log('ethiopia'.toUpperCase()) : countries.push('Ethiopia'); //ETHIOPIA
+
+  //Concatene as duas variáveis ​​a seguir e armazene-as em uma variável fullStack.
+  const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+  const backEnd = ['Node','Express', 'MongoDB']
+  const fullStack = frontEnd.concat(backEnd)
+  console.log(fullStack);//'HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB'
+
+
